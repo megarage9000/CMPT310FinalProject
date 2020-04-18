a1ec528b12df02de16b65dbe439179b6216695bc
jordoyo <john_ordoyo@sfu.ca>
2020-04-17 22:20:42 -0700

    Add Tsp.py

--- project.py
+++ project.py
@@ -1,6 +1,11 @@
 
+import tsp
 
 def helloWorld():
     print("hello World")
+    print("Help me!")
+    result = tsp.load_city_locs("cities1000.txt")
+    for city in result:
+        print(city)
     
 helloWorld()
