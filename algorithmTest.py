9841a1e67e9fb5a250071d84dd2c910d485f7273
jordoyo <john_ordoyo@sfu.ca>
2020-04-20 00:02:39 -0700

    Still fixing bug

--- algorithmTest.py
+++ algorithmTest.py
@@ -9,31 +9,23 @@ import tsp
 class Vertex():
     
     def __init__(self, key):
-        self.adjacent = {}
+        self.adjacent = []
         self.key = key
         
-    def addAdjacent(self, neighbor, weight):
-        self.adjacent[neighbor] = weight
+    def addAdjacent(self, neighbor):
+        self.adjacent.append(neighbor)
         
     def removeAdjacent(self, neighbor):
-        try:
-            del self.adjacent[neighbor]
-        except KeyError:
-            pass
+        self.adjacent.remove(neighbor)
         
-    def getWeightToNeighbor(self, neighbor):
-        value = self.adjacent.get(neighbor)
-        assert value != None
-        return value
-    
     def getID(self):
         return self.key
     
     def getAllAdjacent(self):
-        return self.adjacent.values()
+        return self.adjacent
     
     def toString(self):
-        return "Vertex: " + str(self.key) + ", adjacent to " + str(self.adjacent.values())
+        return "Vertex: " + str(self.key) + ", adjacent to " + str(self.adjacent)
     
 class Edge():
     
@@ -83,22 +75,21 @@ class Graph():
             self.verticesDictionary[key] = vertex
             self.numVertices += 1
         
-    def addEdge(self, vertexA, vertexB, weight):
-        self.getVertex(vertexA.getID()).addAdjacent(vertexB, weight)
-        self.getVertex(vertexB.getID()).addAdjacent(vertexA, weight)   
-        if self.isCycleOnEdgeAdd(vertexA.getID()):
+    def addEdge(self, vertexA, vertexB):
+        self.getVertex(vertexA.getID()).addAdjacent(vertexB)
+        self.getVertex(vertexB.getID()).addAdjacent(vertexA)   
+        if self.isCycleOnEdgeAdd(vertexB.getID()):
             self.deleteEdge(vertexA, vertexB)
         
     def deleteEdge(self, vertexA, vertexB):
         self.getVertex(vertexA.getID()).removeAdjacent(vertexB)
-        self.getVertex(vertexB.getID()).removeAdjacent(vertexB)
+        self.getVertex(vertexB.getID()).removeAdjacent(vertexA)
         
     def addEdgeFromEdge(self, Edge):
         vertices = Edge.getVertices()
-        weight = Edge.getWeight()
         vertexA = self.getVertex(vertices[0])
         vertexB = self.getVertex(vertices[1])
-        self.addEdge(vertexA, vertexB, weight)
+        self.addEdge(vertexA, vertexB)
         
         
     def getAllVertices(self):
@@ -107,9 +98,10 @@ class Graph():
 
     # Cycles here are ones consisting with > 3 edges    
     def isCycle(self, parent, parentOfParent):
-        for child in parent.getAllAdjacent:
+        for child in parent.getAllAdjacent():
             if(child != parent):
                 if(child == parentOfParent or self.isCycle(child, parent)):
+                    print("true!")
                     return True
         return False
 
@@ -117,8 +109,10 @@ class Graph():
         startingVertex = self.getVertex(vertexID)
         children = startingVertex.getAllAdjacent()
         for child in children:
-            if(self.isCycle(child, startingVertex)):
-                return True
+            if(child != startingVertex):
+                if(self.isCycle(child, startingVertex)):
+                    print("True!")
+                    return True
         return False
 
 def test():    
@@ -135,14 +129,14 @@ def test():
             edges.append(edge)
             
     edges.sort()
-    for vertex in graph.getAllVertices():
-        print(vertex.toString())
+    
              
     for edge in edges:
-        # graph.addEdgeFromEdge(edge)
-        print(edge.toString())
-        
-    
+       # graph.addEdgeFromEdge(edge)
+       print(edge.toString())
+       
+    for vertex in graph.getAllVertices():
+        print(vertex.toString())
     
     print("hello")      
   
